3bbb3a395857d4460e8564bd7f5597cfc53c5473
Coalpha <Arcanehood@gmail.com>
2018-09-08 15:16:16 -0700

    alskdja;odjSK:JDhILDhsfiush

--- src/map.ts
+++ src/map.ts
@@ -1,4 +1,6 @@
-import { ArrayFunction } from './types';
+interface ArrayFunction<T> {
+  (ary: Array<any>): Array<T>;
+}
 export default function takeMapFn<T>(fn: () => T) : ArrayFunction<T> {
   return function map(ary: Array<any>) : Array<T> {
     return ary.map(fn);
